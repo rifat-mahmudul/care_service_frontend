8aaaa8676f79995f7298e019acd4b6d4aa9f7f75
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2026-01-17 16:35:25 +0600

    project start

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <div className="flex items-center justify-center min-h-screen flex-col gap-5">
       <h1>Project Will Start</h1>
       <Button>Start Now</Button>
+      <p>start the project</p>
     </div>
   );
 }
