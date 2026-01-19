6da1e31998df4c6827f726c189270f8473e740f9
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2026-01-19 16:40:03 +0600

    navbar for desktop

--- src/components/shared/navbar.tsx
+++ src/components/shared/navbar.tsx
@@ -1,9 +1,68 @@
-import React from 'react'
+"use client";
+import * as React from "react";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu";
+import Image from "next/image";
+import { Button } from "../ui/button";
 
 const Navbar = () => {
   return (
-    <div className='container border border-red-500'>Navbar</div>
-  )
-}
+    <div className="container flex items-center justify-between py-4">
+      {/* logo */}
+      <div>
+        <Image
+          src={"/logo.png"}
+          alt="img.png"
+          width={1000}
+          height={1000}
+          className="h-[60px] w-[60px] object-cover"
+        />
+      </div>
 
-export default Navbar
+      <div>
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>Find Care</NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <NavigationMenuLink>Find Care compo</NavigationMenuLink>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>Find Jobs</NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <NavigationMenuLink>Find Jobs compo</NavigationMenuLink>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>Resources</NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <NavigationMenuLink>Resources compo</NavigationMenuLink>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+          
+          <div className="space-x-5 ml-2">
+            <button>Log in</button>
+
+            <Button className="rounded-3xl h-[45px] w-[120px]">Join Now</Button>
+          </div>
+        </NavigationMenu>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
