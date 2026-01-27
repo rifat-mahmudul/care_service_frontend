c8eedda73d8a5ad0f241bcd5a0d130ec6fb4ee72
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2026-01-27 21:55:32 +0600

    service section done

--- src/components/home/categories.tsx
+++ src/components/home/categories.tsx
@@ -37,6 +37,35 @@ const services1 = [
   },
 ];
 
+const services2 = [
+  {
+    img: "/services/home-garden.png",
+    level: "Home & Garden",
+    link: "/services/home-garden",
+  },
+  {
+    img: "/services/Construction.png",
+    level: "Construction",
+    link: "/services/construction",
+  },
+  {
+    img: "/services/Cleaning.png",
+    level: "Cleaning",
+    link: "/services/cleaning",
+  },
+
+  {
+    img: "/services/senior-care.png",
+    level: "Senior Care",
+    link: "/services/senior-care",
+  },
+  {
+    img: "/services/Medical.png",
+    level: "Medical",
+    link: "/services/medical",
+  },
+];
+
 const Categories = () => {
   return (
     <div className="container space-y-10">
@@ -44,8 +73,8 @@ const Categories = () => {
         One membership connects you to your Global Concierge
       </h1>
 
-      <div>
-        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-6 gap-8 lg:max-w-[1200px] mx-auto">
+      <div className="space-y-8">
+        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-6 gap-8 lg:max-w-[1300px] mx-auto">
           {services1.map((item, index) => (
             <Link key={index} href={item.link}>
               <div className="shadow-[0px_4px_24px_0px_#0000003D] p-4 rounded-lg hover:scale-105 transition-all duration-200">
@@ -58,6 +87,29 @@ const Categories = () => {
                 />
 
                 <h1 className="flex items-center justify-between gap-2 mt-8">
+                  <span>{item?.level}</span>
+                  <span>
+                    <MoveRight />
+                  </span>
+                </h1>
+              </div>
+            </Link>
+          ))}
+        </div>
+
+        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-8 lg:max-w-[1100px] mx-auto">
+          {services2.map((item, index) => (
+            <Link key={index} href={item.link}>
+              <div className="shadow-[0px_4px_24px_0px_#0000003D] p-4 rounded-lg hover:scale-105 transition-all duration-200">
+                <Image
+                  src={item.img}
+                  alt="img.png"
+                  width={1000}
+                  height={1000}
+                  className="object-cover h-[150px] w-fit  mx-auto"
+                />
+
+                <h1 className="flex items-center justify-between mt-8">
                   <span>{item?.level}</span>
                   <span>
                     <MoveRight />
