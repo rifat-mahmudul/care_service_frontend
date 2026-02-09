b3ebfed2dbbdea97563d6dbd02d7fd2183d8a3cf
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2026-02-09 21:27:35 +0600

    banner done

---
+++ src/app/(website)/find-job/page.tsx
@@ -0,0 +1,12 @@
+import Banner from "@/components/shared/Banner/banner";
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="space-y-16">
+      <Banner />
+    </div>
+  );
+};
+
+export default page;
