9cae8c009d81281a7ac4b4568c4a57a94b810c4d
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2026-02-25 01:22:49 +0600

    about section done

--- src/app/(website)/all-find-care/[id]/page.tsx
+++ src/app/(website)/all-find-care/[id]/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { ProfileHero } from "./_components/profile-hero";
+import About from "./_components/about";
 
 const page = () => {
   return (
     <div className="mt-20 space-y-16">
       <ProfileHero />
+      <About />
     </div>
   );
 };
