eef843cd78e36007062e4d4d1d1cdb68abaa4620
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2026-02-26 02:14:08 +0600

    time slot design done

--- src/app/(website)/all-find-care/[id]/page.tsx
+++ src/app/(website)/all-find-care/[id]/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { ProfileHero } from "./_components/profile-hero";
 import About from "./_components/about";
+import BookingComponent from "./_components/booking";
 
 const page = () => {
   return (
     <div className="mt-20 space-y-16">
       <ProfileHero />
       <About />
+      <BookingComponent />
     </div>
   );
 };
