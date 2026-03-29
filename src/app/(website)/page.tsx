5019ccc6735a7f1afc29f6e93e17c7a6124440d3
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2026-03-30 03:16:16 +0600

    city seciton done

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -1,24 +1,24 @@
 import CareSection from "@/components/home/care-section";
 import Categories from "@/components/home/categories";
+import CitySection from "@/components/home/city-section";
 import Faq from "@/components/home/faq";
 import Hero from "@/components/home/hero";
 import MarqueeSection from "@/components/home/marque-section";
 import OurCommunity from "@/components/home/our-community";
 import WhenYou from "@/components/home/when-you";
 import WhyChooseUs from "@/components/home/why-choose-us";
-// import CitySection from "@/components/home/city-section"; 
-// import AcademySection from "@/components/home/academy-section"; 
+// import AcademySection from "@/components/home/academy-section";
 
 export default function Home() {
   return (
     <div className="space-y-16">
       <Hero />
       <Categories />
+      <CitySection />
       <WhenYou />
       <OurCommunity />
       <WhyChooseUs />
-      {/* <CitySection />
-      <AcademySection /> */}
+      {/* <AcademySection /> */}
       <MarqueeSection />
       <Faq />
       <CareSection />
