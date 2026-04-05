2b48beb52d33229e4930cbfdd06dc3e613186cf3
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2026-04-05 19:55:40 +0600

    change password done

---
+++ src/app/(profile)/profile/services/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Services from "./_components/services";
+
+const page = () => {
+  return (
+    <div>
+      <Services />
+    </div>
+  );
+};
+
+export default page;
