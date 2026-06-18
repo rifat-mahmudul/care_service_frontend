de2160e61cf32e2cf3e3ac69f6ed0799c2b62742
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2026-06-18 19:32:31 +0600

    remove marquee seciton

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -2,7 +2,6 @@ import AcademySection from "@/components/home/academy-section";
 import Categories from "@/components/home/categories";
 import CitySection from "@/components/home/city-section";
 import Hero from "@/components/home/hero";
-import MarqueeSection from "@/components/home/marque-section";
 import ProviderSection from "@/components/home/provider-section";
 
 export default function Home() {
@@ -13,13 +12,6 @@ export default function Home() {
       <CitySection />
       <AcademySection />
       <ProviderSection />
-      {/* <WhenYou /> */}
-      {/* <OurCommunity /> */}
-      {/* <WhyChooseUs /> */}
-      <div className="!mb-16">
-        <MarqueeSection />
-      </div>
-      {/* <CareSection /> */}
     </div>
   );
 }
