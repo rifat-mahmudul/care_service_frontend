2f585eae966e42d438e7c1529589323e7841e1f9
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2026-06-18 17:45:54 +0600

    remove secitons from here

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -1,13 +1,10 @@
 import AcademySection from "@/components/home/academy-section";
-import CareSection from "@/components/home/care-section";
 import Categories from "@/components/home/categories";
 import CitySection from "@/components/home/city-section";
 import Faq from "@/components/home/faq";
 import Hero from "@/components/home/hero";
 import MarqueeSection from "@/components/home/marque-section";
 import ProviderSection from "@/components/home/provider-section";
-import WhenYou from "@/components/home/when-you";
-import WhyChooseUs from "@/components/home/why-choose-us";
 
 export default function Home() {
   return (
@@ -17,14 +14,12 @@ export default function Home() {
       <CitySection />
       <AcademySection />
       <ProviderSection />
-      <div>
-        <WhenYou />
-        {/* <OurCommunity /> */}
-        <WhyChooseUs />
-      </div>
+      {/* <WhenYou /> */}
+      {/* <OurCommunity /> */}
+      {/* <WhyChooseUs /> */}
       <MarqueeSection />
       <Faq />
-      <CareSection />
+      {/* <CareSection /> */}
     </div>
   );
 }
